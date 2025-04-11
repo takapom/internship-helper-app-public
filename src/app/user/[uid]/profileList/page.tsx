a0f7c7b78b4f11e555024d32cc4f56294d337af5
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-04-11 22:53:18 +0900

    デバッグ

--- src/app/user/[uid]/profileList/page.tsx
+++ src/app/user/[uid]/profileList/page.tsx
@@ -1,8 +1,6 @@
 import Analysis from "@/components/Analysis"
 import Navbar from "@/components/Navbar"
-import SimpleRadarChart from "@/components/SimpleRadarChart"
 import styles from "./page.module.css"
-import StudyTime from "@/components/StudyTime"
 
 export default function ProfileListPage() {
     return(
@@ -10,8 +8,6 @@ export default function ProfileListPage() {
             <Navbar />
             <h1 className={styles.profile_text}>プロフィール一覧</h1>
             <Analysis />
-            {/* <StudyTime /> */}
-            {/* <SimpleRadarChart /> */}
         </div>
     )
 }
